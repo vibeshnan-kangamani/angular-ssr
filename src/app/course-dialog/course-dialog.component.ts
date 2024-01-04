1683eaf886c0663aed2ef9633ece7734ce0a4e83
Vibeshnan Kanagamani <vibeshnan2301@outlook.com>
2024-01-04 22:59:08 +0530

    Initial SSR Commit

---
+++ src/app/course-dialog/course-dialog.component.ts
@@ -0,0 +1,53 @@
+import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogTitle, MatDialogContent, MatDialogActions } from "@angular/material/dialog";
+import {Course} from "../model/course";
+import { FormBuilder, Validators, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+    selector: 'course-dialog',
+    templateUrl: './course-dialog.component.html',
+    styleUrls: ['./course-dialog.component.css'],
+    standalone: true,
+    imports: [MatDialogTitle, MatDialogContent, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatDialogActions, MatButtonModule]
+})
+export class CourseDialogComponent implements OnInit {
+
+    form: FormGroup;
+    description:string;
+
+    constructor(
+        private fb:FormBuilder,
+        private dialogRef: MatDialogRef<CourseDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) {description,longDescription,
+            category}:Course ) {
+
+        this.description = description;
+
+
+        this.form = fb.group({
+            description: [description, Validators.required],
+            category: [category, Validators.required],
+            longDescription: [longDescription,Validators.required]
+        });
+
+    }
+
+    ngOnInit() {
+
+    }
+
+
+    save() {
+        this.dialogRef.close(this.form.value);
+    }
+
+    close() {
+        this.dialogRef.close();
+    }
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {Course} from "../model/course";
+import {Observable} from "rxjs";
+import {CoursesService} from "../services/courses.service";
+import {map, tap} from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatTabsModule } from '@angular/material/tabs';
+
+@Component({
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    standalone: true,
+    imports: [MatTabsModule, CoursesCardListComponent, AsyncPipe]
+})
+export class HomeComponent implements OnInit {
+
+    courses$: Observable<Course[]>;
+
+    constructor(private coursesService: CoursesService) {
+
+    }
+
+    ngOnInit() {
+        this.courses$ = this.coursesService.findAllCourses()
+            .pipe(
+                map(Object.values)
+            );
+    }
+
+}
